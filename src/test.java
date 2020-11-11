64963bf550fceda572ba40c45f942f5476df27ed
yunpumian <1963085185@qq.com>
2020-11-11 21:34:25 +0800

    yaya

--- src/test.java
+++ src/test.java
@@ -9,5 +9,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("ahahahahahah");
         System.out.println("niangegge");
+        System.out.println("yiyi");
     }
 }

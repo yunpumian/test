29c3c11e276bda7af4f1608de206f0815c0c31ac
yunpumian <1963085185@qq.com>
2020-11-11 21:18:25 +0800

    test

---
+++ src/test.java
@@ -0,0 +1,12 @@
+/**
+ * @author :wn
+ * @program : untitled
+ * @descript :
+ * @create :2020-11-11 21:17
+ */
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("ahahahahahah");
+    }
+}
